0b330f685fb60bf388189718e159bfab3d9c9888
Madeline Carr <madelinencarr@Gmail.com>
2019-05-02 23:44:48 -0400

    Create a hero component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './js/Header';
+import Header from './js/header';
+import Hero from './js/hero';
 
 ReactDOM.render(
     <div>
         <Header />
+        <Hero Title="Hello World" />
     </div>,
     document.getElementById('app')
 );
---
+++ src/js/hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Hero = ({ Title }) => {
+    return (
+        <div className="container">
+            <h2>{Title}</h2>
+        </div>
+    );
+};
+
+export default Hero;
