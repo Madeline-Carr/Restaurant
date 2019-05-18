2a0cee71dd6c9546f3b32b33292e19edbde039f5
Madeline Carr <madelinencarr@Gmail.com>
2019-05-17 23:09:57 -0400

    Placeholder content for features

--- src/js/features.jsx
+++ src/js/features.jsx
@@ -3,7 +3,35 @@ import React from 'react';
 const Features = () => {
     return (
         <div>
-            <p>Features</p>
+            <div className="container">
+                <div className="item">
+                    <h2>Title</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Blanditiis, quasi. Eveniet non quas fuga nostrum ea
+                        deserunt. Neque sapiente, ratione iusto eveniet dolorem
+                        ullam doloremque ipsam ipsum, fugit labore nobis.
+                    </p>
+                </div>
+                <div className="item">
+                    <h2>Title</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Blanditiis, quasi. Eveniet non quas fuga nostrum ea
+                        deserunt. Neque sapiente, ratione iusto eveniet dolorem
+                        ullam doloremque ipsam ipsum, fugit labore nobis.
+                    </p>
+                </div>
+                <div className="item">
+                    <h2>Title</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Blanditiis, quasi. Eveniet non quas fuga nostrum ea
+                        deserunt. Neque sapiente, ratione iusto eveniet dolorem
+                        ullam doloremque ipsam ipsum, fugit labore nobis.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
