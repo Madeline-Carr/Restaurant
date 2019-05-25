a46084d894cc05972e48504169eda89ed7ae73a8
Madeline Carr <madelinencarr@Gmail.com>
2019-05-25 18:52:12 -0400

    Create desserts.jsx

---
+++ src/js/desserts.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import MenuItem from './menuItem';
+
+const DessertMenu = () => {
+    return (
+        <div>
+            <div className="center">
+                <h2 className="darkRed largeText">Desserts</h2>
+                <h3 className="lightRed">
+                    Pizzas are only available in personal sizes.
+                </h3>
+            </div>
+            <div className="container menu">
+                <div className="one">
+                    <ul>
+                        <MenuItem
+                            name="TIRAMISU"
+                            price="12.50"
+                            description="This is a pizza with cheese."
+                        />
+                        <MenuItem
+                            name="CHOCOLATE CAKE"
+                            price="10.50"
+                            description="An icecream scoop can be added for an additional dollar."
+                        />
+                    </ul>
+                </div>
+                <hr />
+                <div className="one">
+                    <ul>
+                        <MenuItem
+                            name="CANNOLI"
+                            price="12.50"
+                            description="This is a pizza with cheese."
+                        />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DessertMenu;
